90648b5fad6777089f394a6e577a6679bb7a042e
HUB <93236246+Guilabriolag@users.noreply.github.com>
2026-01-14 01:36:40 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,14 +3,18 @@ $host = "localhost";
 $db   = "sociallab";
 $user = "root";
 $pass = "";
+$charset = "utf8mb4";
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8",
+        "mysql:host=$host;dbname=$db;charset=$charset",
         $user,
         $pass,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
     );
 } catch (PDOException $e) {
-    die("Erro na conexão");
+    die("Erro de conexão");
 }
